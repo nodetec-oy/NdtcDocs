e964b9e75e7289c45e2199fdebb7e06d33342a8f
Mikhail Koshelev <koshelev.mikhail@outlook.com>
2025-07-21 12:42:07 +0300

    Add GitHub Actions workflow for deploying to GitHub Pages and update Docusaurus configuration for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://nodetec-oy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/NdtcDocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Nodetec Oy', // Usually your GitHub org/user name.
+  organizationName: 'nodetec-oy', // Usually your GitHub org/user name.
   projectName: 'NdtcDocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
